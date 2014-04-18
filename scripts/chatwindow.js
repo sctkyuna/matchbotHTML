eb4e20b24eecbfc06d0d67bb3ba5ba20b26f40f4
Stella <sctkyuna@sctk.(none)>
2014-04-18 01:43:42 -0400

    commented out functions unnecessary to library

--- scripts/chatwindow.js
+++ scripts/chatwindow.js
@@ -1,5 +1,8 @@
+var currentUser  = "You";
+
+
 var chatwindow = {
-    statistics: {
+    /*statistics: {
         frequencies: {},
         nonstoplist_freq: {},
         avg_length: 0,
@@ -25,13 +28,14 @@ var chatwindow = {
         $(".chatwindow").append("<span>" + user + ": </span>" + userinput + "<br>");
         $('input[name=inputtext]').val('');
     },
-
+	*/
     say: function(user, userinput) {
-        $(".prompt").before("<span>" + user + ": </span>" + userinput + "<br>");
+        $(".prompt").before("<span id='username'>" + user + ": </span>" + userinput + "<br>");
         $(".chatwindow").animate({scrollTop: $('.chatwindow').prop("scrollHeight")}, 500);
         $('input[name=inputtext]').val('');
     },
 
+	/*
     userSays: function(userinput) {
         this.say("You", userinput);
     },
@@ -39,6 +43,7 @@ var chatwindow = {
     botSays: function(text) {
         this.say("MatchBot", text);
     },
+	
 
     send_and_get_line: function(userinputobj) {
         //send line of conversation, get response
@@ -54,6 +59,7 @@ var chatwindow = {
         currentype = botresponse.cat;
         this.botSays(botresponse.msg);
     },
+	
 
     calculate: function(userinput, whatthebotsaid) {
         // update statistics
@@ -90,21 +96,21 @@ var chatwindow = {
         chatwindow.statistics.total_words = total;
 
     },
-
+	
     send_data: function() {
         //send statistics to rails
-        /*
-        $.ajax({
-            type: "POST",
-            url: "/AcceptConvo",
-            data: JSON.stringify(chatwindow.statistics),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            failure: function(errMsg) {
-                alert("Connection Error: Chat data not sent");
-            }
-        });
-        */
+        
+        //$.ajax({
+        //    type: "POST",
+        //    url: "/AcceptConvo",
+        //    data: JSON.stringify(chatwindow.statistics),
+        //    contentType: "application/json; charset=utf-8",
+        //    dataType: "json",
+        //    failure: function(errMsg) {
+        //        alert("Connection Error: Chat data not sent");
+        //    }
+        //});
+        
         console.log("in send_data");
     },
 
@@ -113,10 +119,64 @@ var chatwindow = {
         chatwindow.send_data();
         alert("Sweet. Now rerouting you to your matches!");
         window.location.replace("matches.html");
-    },        
+    },
+	*/        
 };
 
+/*
 function isEmail(input) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(input);
 };
+*/
+
+var promptText = "<table class='prompt'><tr> \
+			<td><input id='userinput' name='inputtext' placeholder='>>'/></td> \
+			</tr></table>";
+
+/*var promptText = "<table class='prompt'><tr> \
+			<td><span id='username'>You</span></td> \
+			<td><input id='userinput' name='inputtext' placeholder='Type here'/></td> \
+			</tr></table>";*/
+
+$(document).ready(function() {
+
+	// Adds a .prompt class to the div designated as .chatwindow
+	$(".chatwindow").append(promptText);
+	
+
+    // Message handlers
+    // chatwindow.botSays("Tell me about yourself!");
+    
+    $("#userinput").keypress(function(e) {
+        if (e.which == 13) {
+            var uIn = $('input[name=inputtext]').val();
+            //chatwindow.allresponses.push(uIn);
+            //chatwindow.userSays(uIn);
+            chatwindow.say(currentUser, uIn);
+			/*if (chatwindow.currenttype == "email") {
+                if (isEmail(uIn)) {
+                    chatwindow.email = uIn;
+                    chatwindow.end_seq();
+                }
+                else {
+                    chatwindow.botSays("Oh no! Not an e-mail! Try again?");
+                }
+            }
+            else if (chatwindow.currenttype == "last") {
+                chatwindow.botSays("One last thing - we need your e-mail so that future matches can contact you. What's your e-mail?");
+                chatwindow.currenttype = "email";
+
+            }
+            else {
+                chatwindow.getBot({
+                    msg: uIn,
+                    cat: chatwindow.currenttype
+                });
+                chatwindow.calculate(uIn);
+            }*/
+        }
+    });
+});
+
+
--- scripts/indexscript.js
+++ scripts/indexscript.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
+	// Adds a .prompt class to the div designated as .chatwindow
+	 
+	
+
     // Message handlers
-    chatwindow.botSays("Tell me about yourself!");
+    // chatwindow.botSays("Tell me about yourself!");
     
     $("#userinput").keypress(function(e) {
         if (e.which == 13) {
@@ -33,4 +37,4 @@ $(document).ready(function() {
     });
 });
 
-var stoplist = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","   ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"];
+/*var stoplist = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","   ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"];*/
